2d318aa14a5c95914426d8a1649e3f0a70e8f2ef
sejin.eom <sejin1031@naver.com>
2019-10-02 01:10:30 +0900

    add member file

--- src/components/members/Intern.js
+++ src/components/members/Intern.js
@@ -1,14 +1,12 @@
 import React from 'react';
 import Card from '@material-ui/core/Card';
 import CardContent from '@material-ui/core/CardContent';
-import { Menu, MenuItem, Button } from '@material-ui/core';
-import Intern from './members/Intern';
-import { HashRouter as Router, Route } from 'react-router-dom';
 import { Link as RouterLink } from 'react-router-dom';
 import Link from '@material-ui/core/Link';
+import { Menu, MenuItem, Button } from '@material-ui/core';
 
 
-class Members extends React.Component {
+class Intern extends React.Component {
     render() {
         return (
             <div>
@@ -17,9 +15,20 @@ class Members extends React.Component {
                             Intern
                     </Link>
                 </Button>
+                <Button>
+                    <Link component={RouterLink} to="/members/professor">
+                            professor
+                    </Link>
+                </Button>
+
+                <Card>
+                    <CardContent>
+                        <h1>hasdfasdeelo</h1>
+                    </CardContent>
+                </Card>
             </div>
         );
     }
 }
 
-export default Members;
+export default Intern;
