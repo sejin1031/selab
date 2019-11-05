d54445b9624cb67c9d5ce89f19c489295fec6fed
Lee Jun Seop <ppq23@naver.com>
2019-11-05 10:29:38 +0900

    ??

--- src/components/home/Home.js
+++ src/components/home/Home.js
@@ -1,14 +1,31 @@
 import React from 'react';
 import { Link as RouterLink } from 'react-router-dom';
 import Link from '@material-ui/core/Link';
+import axios from 'axios';
+
 
 import NoticeTable from './NoticeTable';
 import 'react-viewer/dist/index.css';
 import './homeimage.scss';
 
 class Home extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      host : '',
+    }
+  }
+
+  componentDidMount() {
+    this._getHost();
+  }
+
+  _getHost = async() => {
+    const res = await axios.get('/api/host');
+    this.setState({ host : res.data.host })
+  }
+
     render() {
-      
       return (
         <div className="main">
           <img className= "logo" src={require("../selab_logo.png")}/>
@@ -17,7 +34,7 @@ class Home extends React.Component {
             <div className="course">
               <div className="button">
                 <Link component={RouterLink} to="/course/cse326">
-                CSE326 
+                CSE326
                 </Link>
               </div>
             </div>
