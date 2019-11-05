f1d9f7a5cea76a497764f28bd091122de26c2fe7
JunSeop Lee <ppq23@naver.com>
2019-11-05 19:36:25 +0900

    server

--- src/components/home/Home.js
+++ src/components/home/Home.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import { Link as RouterLink } from 'react-router-dom';
 import ReactDom from 'react-dom';
+import axios from 'axios';
+
 
 import 'react-viewer/dist/index.css';
 import './homeimage.scss';
@@ -9,11 +11,20 @@ import './homeimage.scss';
 class Home extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {id: '',pw:''};
+    this.state = {id: '',pw:'',host:''};
 
     this.handleIDChange = this.handleIDChange.bind(this);
     this.handlePWChange = this.handlePWChange.bind(this);
     this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  componentDidMount() {
+    this._getHost();
+  }
+
+  _getHost = async() => {
+    const res = await axios.get('/api/host');
+    this.setState({ host: res.data.host });
   }
   
   handleIDChange(event) {
@@ -37,7 +48,7 @@ class Home extends React.Component {
           <div className="login">
             <form onSubmit={this.handleSubmit}>
             <label>
-              <span className="loginFont">ID</span><br/>
+              <span className="loginFont">{this.state.host}ID</span><br/>
               <input type="text" value={this.state.id} onChange={this.handleIDChange} />
             </label>
             <br/>
