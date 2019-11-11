5108e5c63869e23b87931f2097bfa64f431ebe91
Lee Jun Seop <ppq23@naver.com>
2019-11-11 18:06:14 +0900

    loginserver

--- server/server.js
+++ server/server.js
@@ -2,7 +2,11 @@ const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 4000;
 const db = require('./config/db');
+const bodyParser = require('body-parser')
 
+
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json())
 
 app.get('/api/test', (req, res) => {
     db.query("select * from testtable where 1", (err, data) => {
@@ -14,6 +18,10 @@ app.get('/api/test', (req, res) => {
             res.send(err);
         }
     });
+});
+
+app.post('/add/data', (req, res) => {
+    console.log(req.body)
 });
 
 app.listen(PORT, () => {
