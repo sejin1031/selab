4a3a1d86bc803ca83b503f4a153ae1ad1e4863d6
sejin.eom <sejin1031@naver.com>
2019-11-18 17:02:41 +0900

    add rule for publications

--- src/components/publications/Publications.js
+++ src/components/publications/Publications.js
@@ -1,10 +1,18 @@
 import React from 'react';
 
+var publications = [{title:'Zhiqiang Wu, Xin Chen and Scott Uk-Jin Lee, "Identifying Latent Android Malware from Application'+'s Description using LSTM," In Proc. International Conference on Information, System and Convergence Applications, 2019, pp.40-42',
+                    link:'./publications/[IP]_Zhiqiang1.pdf'},
+                    {title:'',
+                link:''},
+];
+
 class Publication extends React.Component {
     render() {
         return (
-            <div>
-                <h1>hello</h1>
+            <div className="publicationContent">
+                <div className="publications">
+
+                </div>
             </div>
         );
     }
