52a15271cc8b7171ccfbf4ccd5856bfee6fb0859
Lee Jun Seop <ppq23@naver.com>
2019-11-19 14:28:45 +0900

    notice fix

--- src/components/notice/Notice.js
+++ src/components/notice/Notice.js
@@ -17,29 +17,6 @@ class Notice extends React.Component {
         showPopup: false,
         maxNo: 4,
         test : [],
-        boards: [
-            {
-                brdno: 1,
-                brdwriter: 'Scott lee',
-                brdtitle: '공지사항',
-                brdcontent: '오늘 수업 없음',
-                brddate: new Date()
-            },
-            {
-                brdno: 2,
-                brdwriter: 'hyun soo',
-                brdtitle: '게시판',
-                brdcontent: '이거만들라면 db랑 서버랑 연동시켜야함',
-                brddate: new Date()
-            },
-            {
-                brdno: 3,
-                brdwriter: 'umsejin',
-                brdtitle: 'web app master',
-                brdcontent: '그는 금요일에 안해온 놈들의 뚝배기를 깨버릴것이다',
-                brddate: new Date()
-            }
-        ],
          selectedBoard:{}
       };
       this.noticeLoad();
@@ -63,7 +40,7 @@ class Notice extends React.Component {
    
 
   render() {
-    const { boards, selectedBoard } = this.state;
+    const { selectedBoard } = this.state;
     return (
       <div className='app'>
 <div>test code id={this.props.id}</div>
@@ -81,11 +58,7 @@ class Notice extends React.Component {
               </tr>
             </thead>
             <tbody>
-              {
-                  boards.map(row =>
-                      (<BoardItem key={row.brdno} row={row} onRemove={this.handleRemove} onSelectRow={this.handleSelectRow} />)
-                  )
-              }
+              
               {this.state.test.map(data => <tr>
                 <td>{data.id}</td>
                 <td>{data.title}</td>
