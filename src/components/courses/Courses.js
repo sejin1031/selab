f146ba1df69639efa804d3768f0e8973ed219b41
Jongmin Lee <darkcruel12@naver.com>
2019-12-19 13:34:49 +0900

    modify color in course

--- src/components/courses/Courses.js
+++ src/components/courses/Courses.js
@@ -5,27 +5,27 @@ import "./courses.css"
 class Course extends React.Component {
      render() { 
         return (
-            <div class = "Course">
+            <div className = "Course">
                <h1 id='course_title'>Course</h1>
 
                <br />
 
-               <div class="under_courses">
+               <div className="under_courses">
                   <img src= {require("./hanyang_university.png")}/>
-                   <h2> Undergraduate Courses</h2>
+                   <h2 id = "course_content_title"> Undergraduate Courses</h2>
                    <ul id = "under_list">
-                     <li id = "course_list"><Link to="/course/cse326">CSE326:Web Application Development</Link></li>
+                     <li id = "course_list"><Link to="/course/cse326" style={{textDecoration:'none',color:'white'}}>CSE326:Web Application Development</Link></li>
                      <li id = "course_list">CSE107:Logical Fundamentals of Programming</li>
                      <li id = "course_list">CSE117:Programming Fundamentals</li>
                      <li id = "course_list">CSE322:Web Programming</li>
                      <li id = "course_list">CSE406:Software Engineering</li>
                    </ul>
                 </div>
-               <div class="line">
+               <div className="line">
                </div>
-               <div class="post_courses">
+               <div className="post_courses">
                   <img src= {require("./hanyang_university.png")}/>
-                  <h2> Postgraduate Courses</h2>
+                  <h2 id = "course_content_title"> Postgraduate Courses</h2>
                   <ul id="post_list">
                     <li id = "course_list">CIN870:Formal Methods</li>
                     <li id = "course_list">CSE603:Management of Semistructured Data</li>
